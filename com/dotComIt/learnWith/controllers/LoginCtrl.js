d0dc3e0ac3c5d7e10f0237480dec87eadcaafa03
Nathan Melo <nathan.vix@gmail.com>
2017-05-20 11:04:32 +0200

    First commit

--- com/dotComIt/learnWith/controllers/LoginCtrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('learnWith').controller('LoginCtrl', ['$scope',function($scope){
-    $scope.title = 'Login View';
-}]);
--- com/dotComIt/learnWith/controllers/MainScreenCtrl.js
+++
@@ -1,4 +0,0 @@
-
-angular.module('learnWith').controller('MainScreenCtrl', ['$scope',function($scope){
-    $scope.title = 'Main View';
-}]);
